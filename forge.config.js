a822d8fc826e9620e4bcc2c9f53ff8df340f4116
George M <gsmaroun@gmail.com>
2023-02-15 18:50:18 -0500

    first commit: electron app for nimbus

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+module.exports = {
+  packagerConfig: {
+    icon: './icon/nimbus-app-logo',
+    osxSign: {
+      "identity": "Developer ID Application: George Maroun (26HM4GD569)"
+    },
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL("http://nimbusapp-env.eba-jwatv44i.us-east-1.elasticbeanstalk.com/");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("resize", function (e, x, y) {
+  mainWindow.setSize(x, y);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
